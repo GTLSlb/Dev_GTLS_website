b700f3496aa222660532f7a180a646e8b10614fa
ahmadbsat153 <ahmadb@gtls.com.au>
2024-05-02 15:27:23 +0300

    Added titles to the Business Executive job

--- resources/js/Pages/Component/opportunities/BusinessExecutive.jsx
+++ resources/js/Pages/Component/opportunities/BusinessExecutive.jsx
@@ -3,16 +3,19 @@ import React from "react";
 function BusinessExecutive() {
     return (
         <div className="mt-4">
+            <p className="font-bold text-smooth">Role Description.</p>
             <p className="mt-2 text-gray-300">
-                As a Business Development Executive, you will be responsible for
-                identifying and securing new business opportunities, generating
-                leads, and managing client accounts. You will be expected to
-                build strong relationships with potential clients, understand
-                their needs, and present our logistics solutions to meet their
-                requirements. This role will require regular collaboration with
-                the sales and operations teams to ensure seamless customer
-                experiences.
+                This is a full-time on-site role for a Business Development
+                Executive. As a Business Development Executive, you will be
+                responsible for identifying and securing new business
+                opportunities, generating leads, and managing client accounts.
+                You will be expected to build strong relationships with
+                potential clients, understand their needs, and present our
+                logistics solutions to meet their requirements. This role will
+                require regular collaboration with the sales and operations
+                teams to ensure seamless customer experiences.
             </p>
+            <p className="font-bold text-smooth mt-4">Qualifications.</p>
             <ul role="list" className="mt-4 max-w-xl space-y-2 text-gray-300">
                 <li className="flex gap-x-3 items-center">
                     <div className=" h-2 w-2 flex-none rounded-full bg-goldt" />
@@ -66,7 +69,9 @@ function BusinessExecutive() {
                 </li>
                 <li className="flex gap-x-3 items-center">
                     <div className=" h-2 w-2 flex-none rounded-full bg-goldt" />
-                    <span>Experience in the logistics industry is a requirement</span>
+                    <span>
+                        Experience in the logistics industry is a requirement
+                    </span>
                 </li>
             </ul>
         </div>
