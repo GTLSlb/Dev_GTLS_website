d40fb7eb0b999a17448a35f1856e3d9b1d15536a
ahmadbsat153 <ahmadb@gtls.com.au>
2024-09-20 11:57:40 +0300

    Fixes

--- resources/js/Components/Navbars.jsx
+++ resources/js/Components/Navbars.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect, useRef, useLayoutEffect } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { PhoneIcon } from "@heroicons/react/20/solid";
-import goldTigerLogo from "../../assets/pictures/goldTigerLogo.webp";
 import LogoWhite from "../../assets/pictures/LogoWhite.webp";
 import { Link } from "@inertiajs/inertia-react";
 import { Popover, Transition } from "@headlessui/react";
