bc325423bfa3ac1aed0f28e7dd98119e970801d6
ahmadBsat <ahmad.bsat.15.3@gmail.com>
2025-08-14 12:46:05 +0300

    fix: add minimum height to ContactUs component for better layout

--- resources/js/Pages/Component/ContactUsComp/ContactUs.jsx
+++ resources/js/Pages/Component/ContactUsComp/ContactUs.jsx
@@ -4,7 +4,7 @@ import ContactUsHubspot from "./ContactUsHubspot";
 export default function ContactUs() {
     return (
         <div className="py-16">
-            <div className=" lg:max-w-7xl mx-auto w-full">
+            <div className=" lg:max-w-7xl mx-auto w-full min-h-[80vh]">
                 {/* <ContactUsForm /> */}
                 <ContactUsInfo />
                 <ContactUsHubspot />
